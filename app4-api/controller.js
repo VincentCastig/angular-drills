0d2781c34617d29da327f8aa76c363a093c54f8f
Vincent Castigliola <vincentcastigliola@Vincents-MacBook-Air.local>
2017-07-20 14:43:14 -0500

    api1

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -2,7 +2,7 @@ angular.module('apiApp').controller('myCtrl', function($scope, myService){
   $scope.test = myService.test
   myService.getPlanets()
     .then(function(response){
-      $scope.planet = response
+      $scope.planet = response.data.results
       console.log(response)
     })
     .catch(function(response){
--- app4-api/service.js
+++ app4-api/service.js
@@ -5,7 +5,7 @@ angular.module('apiApp').service('myService', function($http){
   this.getPlanets = function(){
     return $http.get('http://www.swapi.co/api/planets')
       .then(function(response){
-        return response.data.results
+        return response
       })
   }
 
